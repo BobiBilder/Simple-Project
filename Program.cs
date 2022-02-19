344b683ed9439830792af8a8b212b0fafb77334a
tomer bilder <bilder52@outlook.com>
2022-02-19 19:20:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace Tirgul
+{
+    class Program
+    {
+        //helpers
+        public static Rational Maker()
+        {
+            Console.Write("Enter a numerator: ");
+            int numerator = int.Parse(Console.ReadLine());
+            Console.Write("Enter a denominator: ");
+            int denominator = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            return new Rational(numerator, denominator);
+        } //Making Rationals, makes the code shorter
+        public static bool IsPrime(int x)
+        {
+            bool prime = true;
+            if (x == 1 || x == 2 || x == 3)
+            {
+                return true;
+            }
+            for (int i = 2; i < Math.Sqrt(x); i++)
+            {
+                if (x % i == 0)
+                {
+                    prime = false;
+                }
+            }
+            return prime;
+        } //Chcking if a number is a prime number
+
+        //Questions
+        public static void Q2()
+        {
+            Rational rat1 = Maker();
+            Rational rat2 = Maker();
+            Console.WriteLine(rat1.ToString() + " + " + rat2.ToString() + " = " + rat1.Addition(rat2));
+            Console.WriteLine(" | " + rat1.ToString() + " - " + rat2.ToString() + " | " + " = " + rat1.Difference(rat2));
+            Console.WriteLine(rat1.ToString() + " * " + rat2.ToString() + " = " + rat1.Multiply(rat2));
+            Console.WriteLine(rat1.ToString() + " / " + rat2.ToString() + " = " + rat1.Divide(rat2));
+            Console.WriteLine(rat1.ToString() + " => " + rat1.Decimal());
+            Console.WriteLine(rat2.ToString() + " => " + rat2.Decimal());
+        }
+        public static void Q3()
+        {
+            Console.Write("Enter the amount of rationals:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Rational sum = Maker();
+            for (int i = 1; i < n; i++)
+            {
+                Rational rat = Maker();
+                sum = sum.Addition(rat);
+            }
+            Console.WriteLine(sum.ToString());
+        }
+        public static void Q4()
+        {
+            Console.Write("Enter the amount of rationals:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Rational min = Maker();
+            Rational max = min;
+            for (int i = 1; i < n; i++)
+            {
+                Rational rati = Maker();
+                if (rati.Decimal() > max.Decimal())
+                {
+                    max = rati;
+                }
+                if (rati.Decimal() < min.Decimal())
+                {
+                    min = rati;
+                }
+            }
+            Console.WriteLine("The Biggest Rational: " + max.ToString());
+            Console.WriteLine("The smalles Rational: " + min.ToString());
+        }
+        public static void Q5()
+        {
+            int ans = 0;
+            Rational rat = Maker();
+            while (rat.GetDenominator() != 0)
+            {
+                Rational rati = Maker();
+                if (rati.Decimal() < rat.Decimal())
+                {
+                    ans++;
+                }
+                rat.SetDenominator(rati.GetDenominator());
+                rat.SetNumerator(rati.GetNumerator());
+            }
+            Console.WriteLine(ans);
+        }
+        public static void Q6()
+        {
+            Console.Write("Enter the amount of rationals:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            string ans = "";
+            for (int i = 0; i < n; i++)
+            {
+                Rational rat = Maker();
+                rat.Assembler();
+                if (IsPrime(rat.GetNumerator()) || IsPrime(rat.GetDenominator()))
+                {
+                    if (i < n - 1)
+                    {
+                        ans = ans + rat.ToString() + ", ";
+                    }
+                    else
+                    {
+                        ans = ans + rat.ToString() + ".";
+                    }
+                }
+            }
+            Console.WriteLine(ans);
+        }
+
+        public static void QAll()
+        {
+            Console.WriteLine("Q2");
+            Q2();
+            Console.WriteLine("\r\n" + "Q3");
+            Q3();
+            Console.WriteLine("\r\n" + "Q4");
+            Q4();
+            Console.WriteLine("\r\n" + "Q5");
+            Q5();
+            Console.WriteLine("\r\n" + "Q6");
+            Q6();
+        } //all the questions together, makes the code shorter
+        static void Main(string[] args)
+        {
+            QAll();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rational.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Tirgul
+{
+    class Rational
+    {
+        //feild
+        int numerator;
+        int denominator;
+        int integer;
+
+        //builder
+        public Rational(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+            Divider();
+            Simplifier();
+        }
+
+        //functions
+        /**adds a rational with another rational
+         * a -> rational to add with
+         */
+        public Rational Addition(Rational a)
+        {
+            Helper1(a);
+            int newDenominator = a.denominator * this.denominator;
+            int newNumerator = a.numerator * this.denominator + a.denominator * this.numerator;
+            Rational ans = new Rational(newNumerator, newDenominator);
+            Helper2(a, ans);
+            return ans;
+        }
+        /**subtracts a rational with another rational
+         * a -> rational to sabtracts with
+         */
+        public Rational Difference(Rational a)
+        {
+            Helper1(a);
+            int newDenominator = a.denominator * this.denominator;
+            int newNumerator =Math.Max(a.numerator * this.denominator, a.denominator * this.numerator) - Math.Min(a.denominator * this.numerator, a.numerator * this.denominator) ;
+            Rational ans = new Rational(newNumerator, newDenominator);
+            Helper2(a, ans);
+            return ans;
+        }
+        /**multiplies a rational with another rational
+         * a -> rational to multiply with
+         */
+        public Rational Multiply(Rational a)
+        {
+            Helper1(a);
+            int newDenominator = a.denominator * this.denominator;
+            int newNumerator = a.numerator * this.numerator;
+            Rational ans = new Rational(newNumerator, newDenominator);
+            Helper2(a, ans);
+            return ans;
+        }
+
+        /**divides a rational with another rational
+         * a -> rational to divide with
+         */
+        public Rational Divide(Rational a)
+        {
+            Helper1(a);
+            int newNumerator = this.numerator * a.denominator;
+            int newDenominator = this.denominator * a.numerator;
+            Rational ans = new Rational(newNumerator, newDenominator);
+            Helper2(a, ans);
+            return ans;
+        }
+
+
+        public double Decimal()
+        {
+            return (double)integer + ((double)numerator / (double)denominator);
+        }
+
+        //tostring
+        override
+        public string ToString()
+        {
+            if(numerator != 0){
+                if (this.integer > 0)
+                {
+                    string ans = this.integer + " " + this.numerator + ":" + this.denominator;
+                    return ans;
+                }
+                else
+                {
+                    string ans = this.numerator + ":" + this.denominator;
+                    return ans;
+                }
+            }
+            else
+            {
+                if (this.integer > 0)
+                {
+                    string ans = integer.ToString() + ".0";
+                    return ans;
+                }
+                else
+                {
+                    return "0";
+                }
+            }
+        }
+
+
+
+
+        //helpers
+        private void Simplifier()
+        {
+            int gcd = 1;
+            for (int i = 2; i <= Math.Min(numerator,denominator); i++)
+            {
+                if(numerator % i == 0 && denominator % i == 0 && i > gcd)
+                {
+                    gcd = i;
+                }
+            }
+            numerator = numerator / gcd;
+            denominator = denominator / gcd;
+        }  //simplifing the rational
+        public void Assembler()
+        {
+            numerator = numerator + (integer * denominator);
+            integer = 0;
+        }    //assembling the integers into the rational
+        private void Divider()
+        {
+            Assembler();
+            if (denominator != 0)
+            {
+                if (numerator / denominator >= 1)
+                {
+                    integer = numerator / denominator;
+                    numerator = numerator - (integer * denominator);
+                }
+            }
+        }     //dividing the integer from the rational
+        private void Helper1(Rational a)
+        {
+            Assembler();
+            a.Assembler();
+        }  //helping to assemble, makes the code shorter
+        private void Helper2(Rational a, Rational ans)
+        {
+            ans.Divider();
+            ans.Simplifier();
+            Divider();
+            a.Divider();
+            Simplifier();
+            a.Simplifier();
+        } //helping to divide and simplify, makes the code shorter
+
+
+        //getters
+        public int GetDenominator()
+        {
+            return this.denominator;
+        }
+        public int GetNumerator()
+        {
+            return this.numerator;
+        }
+
+        //setters
+        public void SetDenominator(int denominator)
+        {
+            this.denominator = denominator;
+        }
+        public void SetNumerator(int numerator)
+        {
+            this.numerator = numerator;
+        }
+    }
+}
